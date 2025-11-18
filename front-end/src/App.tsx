94774da3dd848d7998d21afdddcda43918ffc698
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-18 15:51:22 +0200

    feat: changes made in App.tsx for routes/pages

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,32 +1,18 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/footer/Footer";
 import Navbar from "./components/NavBar/Navbar";
+import UserLogin from "./pages/user/UserLogin";
 import Landing from "./pages/Landing";
-// Layout Component
-const Layout = () => (
-  <>
-    <Navbar />
-    <Outlet /> 
-    <Footer />
-  </>
-);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Landing />} />
-          <Route path="admin" element={<Outlet />}>
-          </Route>
-          <Route path="user" element={<Outlet />}>
-          </Route>
-        </Route>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<UserLogin />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
