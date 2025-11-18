eda709cd7aa1b94e116ae33d4b54481f5293f949
Sbonelo2 <cbonelo224@gmail.com>
2025-11-18 15:23:28 +0200

    feat: NavProfile

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,41 +1,25 @@
-<<<<<<< HEAD
 import './App.css'
 import HeroSection from './components/heroSection/HeroSection'
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Navbar from './components/NavBar/Navbar';
 import InputFields from './components/InputFields/InputFields';
+import NavProfile from './components/NavProfile/NavProfile';
 
-=======
-import { useState } from "react";
-import "./App.css";
-import HeroSection from "./components/heroSection/HeroSection";
-import Footer from "./components/footer/Footer";
-import Snackbar from "./components/Snackbar/snackbar";
-import Navbar from "./components/NavBar/Navbar";
->>>>>>> dev
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-     <HeroSection/>
-        
-      < Navbar />
-      <Footer />
-      <InputFields />
-      
-=======
       <HeroSection />
 
-=======
-     <Snackbar/>
->>>>>>> f70acc0e24f6e0beab9bf4bd55f97d1e414fcec1
       <Navbar />
-       <HeroSection />
+      <NavProfile />
       <Footer />
->>>>>>> dev
+      <InputFields />
+
+      <Navbar />
+      <HeroSection />
+      <Footer />
     </>
   );
 }
