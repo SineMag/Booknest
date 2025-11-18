04cb756d40383ab6e3f17f18524ce5b509faecf4
Mluleki <melokuhlemluleki02@gmail.com>
2025-11-18 11:56:44 +0200

    Refactor Image component to ImageUploader: shorten code, add CSS styling, update imports and remove duplicate usage

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import './App.css'
-import HeroSection from './components/heroSection/HeroSection'
-=======
+import { useState } from "react";
+import "./App.css";
+import HeroSection from "./components/heroSection/HeroSection";
 import "./App.css";
 import Footer from "./components/footer/Footer";
-import Navbar from './components/NavBar/Navbar'
->>>>>>> e660a8a229faa1a39ee5885c632e995c31ecef54
-
+import Navbar from "./components/NavBar/Navbar";
+import Image from "./components/image/ImageUploader";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-     <HeroSection/>
-        
-=======
-      < Navbar />
+      <HeroSection />
+
+      <Navbar />
       <Footer />
->>>>>>> e660a8a229faa1a39ee5885c632e995c31ecef54
     </>
   );
 }
