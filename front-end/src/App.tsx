8537d8fdb3ccc451562b263a0594dd1437335f72
Shantela21 <shantelaslie@gmail.com>
2025-11-18 14:19:10 +0200

    removed unused imports

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css'
 import HeroSection from './components/heroSection/HeroSection'
 import "./App.css";
@@ -6,36 +5,23 @@ import Footer from "./components/footer/Footer";
 import Navbar from './components/NavBar/Navbar';
 import InputFields from './components/InputFields/InputFields';
 
-=======
-import { useState } from "react";
-import "./App.css";
-import HeroSection from "./components/heroSection/HeroSection";
-import Footer from "./components/footer/Footer";
-import Snackbar from "./components/Snackbar/snackbar";
-import Navbar from "./components/NavBar/Navbar";
->>>>>>> dev
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-     <HeroSection/>
+    
+
+     {/* <HeroSection/>
         
       < Navbar />
       <Footer />
       <InputFields />
-      
-=======
-      <HeroSection />
-
-=======
-     <Snackbar/>
->>>>>>> f70acc0e24f6e0beab9bf4bd55f97d1e414fcec1
+       */}
       <Navbar />
        <HeroSection />
+       {/* <Snackbar */}
       <Footer />
->>>>>>> dev
+
     </>
   );
 }
