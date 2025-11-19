7cd8d415349308745a14303e84c15f5e3975783b
Msizi <msizimwelase007@gmail.com>
2025-11-19 09:53:53 +0200

    fix: recreated login, removed login form and fixed input field component

--- front-end/src/pages/user/UserLogin.tsx
+++ front-end/src/pages/user/UserLogin.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import InputField from "../../components/InputField/InputField";
 import Button from "../../components/Button/Button";
-import { Link } from "react-router-dom";
-import LoginForm from "../../components/LoginForm/LoginForm";
 
 const UserLogin: React.FC = () => {
+  // states
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <LoginForm />
-        <Button onClick={() => console.log("User Login clicked")} variant="primary">
-          Login
-        </Button>
-      
-      
+    <div className="loginPage">
+      <div className="loginContainer">
+        <InputField
+          placeholder="Email"
+          type="email"
+          field={username}
+          setField={setUsername}
+        />
+        <InputField
+          placeholder="Password"
+          type="email"
+          field={password}
+          setField={setPassword}
+        />
+        <Button variant="primary">Login</Button>
+      </div>
     </div>
   );
 };
