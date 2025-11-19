55359a320d8eec9cc5b27360e2a16281dac31bca
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-19 08:49:37 +0200

    fix: created a login.module.css file to fix a bug in a form component

--- front-end/src/components/LoginForm/LoginForm.tsx
+++ front-end/src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "../LoginForm/LoginForm.module.css";
+import styles from "./LoginForm.module.css";
 
 interface LoginFormData {
   email: string;
