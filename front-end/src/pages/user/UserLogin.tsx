52cf0806fd8d61196e954f48680fc1ff00c4db6d
Msizi <msizimwelase007@gmail.com>
2025-11-19 10:13:56 +0200

    fix: added responsiveness to the login Page and adjusted padding and size for different screens

--- front-end/src/pages/user/UserLogin.tsx
+++ front-end/src/pages/user/UserLogin.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import InputField from "../../components/InputField/InputField";
 import Button from "../../components/Button/Button";
+import Navbar from "../../components/NavBar/Navbar";
+import Footer from "../../components/footer/Footer";
 
 const UserLogin: React.FC = () => {
   // states
@@ -8,23 +10,28 @@ const UserLogin: React.FC = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <div className="loginPage">
-      <div className="loginContainer">
-        <InputField
-          placeholder="Email"
-          type="email"
-          field={username}
-          setField={setUsername}
-        />
-        <InputField
-          placeholder="Password"
-          type="email"
-          field={password}
-          setField={setPassword}
-        />
-        <Button variant="primary">Login</Button>
+    <>
+      <Navbar />
+      <div className="loginPage">
+        <div className="loginContainer">
+          <h2 style={{ textAlign: "center", marginBottom: "1rem" }}>Login</h2>
+          <InputField
+            placeholder="Email"
+            type="email"
+            field={username}
+            setField={setUsername}
+          />
+          <InputField
+            placeholder="Password"
+            type="email"
+            field={password}
+            setField={setPassword}
+          />
+          <Button variant="primary">Login</Button>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
