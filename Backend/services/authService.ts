43f9d5e53b409d1beed7443aa011601d72829e45
Msizi <msizimwelase007@gmail.com>
2025-11-20 10:06:17 +0200

    added /auth endpoint

---
+++ Backend/services/authService.ts
@@ -0,0 +1,16 @@
+import {
+  LoginCredentials,
+  User,
+  insertUserQuery,
+  loginUserQuery,
+} from "../models/User";
+
+export const insertIntoUsers = async (user: User) => {
+  const result = await insertUserQuery(user);
+  return result;
+};
+
+export const loginUserDB = async (credentials: LoginCredentials) => {
+  const result = await loginUserQuery(credentials);
+  return result;
+};
