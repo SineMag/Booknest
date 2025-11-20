ca859a1af97b012db20794371737eab26c1e4a91
Shantela21 <shantelaslie@gmail.com>
2025-11-20 08:32:34 +0200

    halfway through slice

--- front-end/src/features/userSlice.ts
+++ front-end/src/features/userSlice.ts
@@ -1,5 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
+export interface User {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  physicalAddress: string;
+  phoneNumber: string;
+  profilePicUrl?: string;
+  password: string;
+  createdAt?: Date;
+}
+
+
+export const createUser = createAsyncThunk(
+  "user/createUser",
+  async (user: User, {rejectWithValue}) => {
+    try {
+      const response = await axios.post('https://booknestbackend-lljq.onrender.com/users', user);
+      if (response.status === 201)
+      return response.data;
+    else return rejectWithValue(response.data.message);
+    } catch (error: any) {
+      return rejectWithValue(error.response?.data?.message || error.message);
+    }
+
+  })
+ 
 const initialState = {
   user: null,
 };
@@ -8,6 +36,14 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(createUser.fulfilled, (state, action) => {
+      state.user = action.payload;
+    });
+    builder.addCase(createUser.rejected, (state, action) => {
+      console.log(action.payload);
+    });
+  },
 });
 
 export default userSlice.reducer;
