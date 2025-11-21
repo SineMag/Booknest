e139007644538a31e162196d41a452d35994c22c
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-21 12:03:19 +0200

    chore: Untrack node_modules and dist directories

--- Backend/dist/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authController_1 = require("../controllers/authController");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post("/register", authController_1.createUser);
-router.post("/login", authController_1.loginUser);
-exports.default = router;
-//# sourceMappingURL=authRouter.js.map
