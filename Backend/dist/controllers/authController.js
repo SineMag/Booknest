f6840c34d4b47d6ec60705737aa8c2fa79482706
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-21 12:35:33 +0200

    feat: created accomodatio's tags

--- Backend/dist/controllers/authController.js
+++ Backend/dist/controllers/authController.js
@@ -1,26 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginUser = exports.createUser = void 0;
-const authService_1 = require("../services/authService");
-const console_1 = require("console");
-const createUser = async (req, res) => {
+import { insertIntoUsers, loginUserDB } from "../services/authService";
+import { Request, Response } from "express";
+import { log } from "console";
+export const createUser = async (req, res) => {
     try {
-        const user = await (0, authService_1.insertIntoUsers)(req.body);
+        const user = await insertIntoUsers(req.body);
         return res.status(201).json(user);
     }
     catch (error) {
-        (0, console_1.log)(error);
+        log(error);
     }
 };
-exports.createUser = createUser;
-const loginUser = async (req, res) => {
+export const loginUser = async (req, res) => {
     try {
-        const user = await (0, authService_1.loginUserDB)(req.body);
+        const user = await loginUserDB(req.body);
         return res.status(200).json(user);
     }
     catch (error) {
-        (0, console_1.log)(error);
+        log(error);
     }
 };
-exports.loginUser = loginUser;
 //# sourceMappingURL=authController.js.map
--- Backend/dist/services/authService.js
+++ Backend/dist/services/authService.js
@@ -1,15 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginUserDB = exports.insertIntoUsers = void 0;
-const User_1 = require("../models/User");
-const insertIntoUsers = async (user) => {
-    const result = await (0, User_1.insertUserQuery)(user);
+import { LoginCredentials, User, insertUserQuery, loginUserQuery, } from "../models/User";
+export const insertIntoUsers = async (user) => {
+    const result = await insertUserQuery(user);
     return result;
 };
-exports.insertIntoUsers = insertIntoUsers;
-const loginUserDB = async (credentials) => {
-    const result = await (0, User_1.loginUserQuery)(credentials);
+export const loginUserDB = async (credentials) => {
+    const result = await loginUserQuery(credentials);
     return result;
 };
-exports.loginUserDB = loginUserDB;
 //# sourceMappingURL=authService.js.map
