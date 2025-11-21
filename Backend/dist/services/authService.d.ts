b5b93229f4cdbfb03005c89a16ae136be4faccdd
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-21 13:03:15 +0200

    chore: merged dev with feat/ReviewCard

---
+++ Backend/dist/services/authService.d.ts
@@ -0,0 +1,4 @@
+import { LoginCredentials, User } from "../models/User";
+export declare const insertIntoUsers: (user: User) => Promise<Record<string, any>[]>;
+export declare const loginUserDB: (credentials: LoginCredentials) => Promise<Record<string, any>[]>;
+//# sourceMappingURL=authService.d.ts.map
