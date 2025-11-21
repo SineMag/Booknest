a6f2852a0f881e1ff4c2e141745fa08875afcc16
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-21 12:39:14 +0200

    feat: created a favourites' tag/icon

---
+++ Backend/dist/controllers/UserController.d.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from "express";
+export declare const getAllUsers: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+export declare const getUserById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+export declare const updateUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+//# sourceMappingURL=UserController.d.ts.map
