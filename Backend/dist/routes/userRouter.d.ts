361e38fa8f802a9bbd4fb57aa25d4752b0cbc8ce
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-24 09:11:18 +0200

    feat: added the routes in backend

--- Backend/dist/routes/userRouter.d.ts
+++ Backend/dist/routes/userRouter.d.ts
@@ -1,7 +1,3 @@
 declare const router: import("express-serve-static-core").Router;
 export default router;
-<<<<<<< HEAD
-//# sourceMappingURL=UserRouter.d.ts.map
-=======
 //# sourceMappingURL=userRouter.d.ts.map
->>>>>>> feat/ReviewCard
