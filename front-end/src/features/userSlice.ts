01248c3a74fc56f28879af8698b5342af7aefea8
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-24 10:54:56 +0200

    fix: replaced the localhost storage with netlify web server hosted site

--- front-end/src/features/userSlice.ts
+++ front-end/src/features/userSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://booknestapi.netlify.app/";
 
 export interface User {
   id?: number;
