1def702db92d79696267af6140615b8e90d2c8d5
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-26 15:49:25 +0200

    feat: deleted dist folder

--- Backend/dist/services/authService.d.ts
+++
@@ -1,4 +0,0 @@
-import { type LoginCredentials, type User } from "../models/User";
-export declare const insertIntoUsers: (user: User) => Promise<Record<string, any>[]>;
-export declare const loginUserDB: (credentials: LoginCredentials) => Promise<Record<string, any>[]>;
-//# sourceMappingURL=authService.d.ts.map
