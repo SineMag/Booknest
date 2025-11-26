8a8523272b6e0a0c077e7fe9ec0f6a8670a4564d
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-26 15:39:32 +0200

    fix: deleted dist from backend folder

--- Backend/dist/services/UserService.d.ts
+++
@@ -1,5 +0,0 @@
-import { type User } from "../models/User";
-export declare const selectAllUsers: () => Promise<Record<string, any>[]>;
-export declare const selectUserById: (id: number) => Promise<Record<string, any>[]>;
-export declare const putIntoUser: (id: number, user: User) => Promise<Record<string, any>[]>;
-//# sourceMappingURL=UserService.d.ts.map
