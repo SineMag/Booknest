785a91561e256bf000f6afeefcd5f4cab6b6ea1b
Mluleki <melokuhlemluleki02@gmail.com>
2025-11-27 08:33:49 +0200

    ...

--- front-end/src/pages/Error404.tsx
+++ front-end/src/pages/Error404.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../components/Button/Button';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../components/Button/Button";
+import styles from "./Error404.module.css";
+import Navbar from "../components/NavBar/Navbar";
+import Footer from "../components/footer/Footer";
 
-const Error404: React.FC = () => {
+const Error404 = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
+   <div> <Navbar /> 
+    < div className={styles.container}>
+      <h1 className={styles.code}>404</h1>
 
-      <h1 style={{ fontSize: '5rem', fontWeight: 'bold' }}>404 PAGE NOT FOUND</h1>
-      <br />
-      <Link to="/">
-        <Button variant="primary">Go back to home</Button>
+      <h2 className={styles.title}>Page Not Found</h2>
+
+      <p className={styles.text}>
+
+        It looks like nothing was found at this location.
+        
+      </p>
+
+      <Link to="/" style={{ marginTop: "30px" }}>
+        <Button variant="primary">BACK TO HOME</Button>
       </Link>
+    </div>
+    <Footer />
     </div>
   );
 };
