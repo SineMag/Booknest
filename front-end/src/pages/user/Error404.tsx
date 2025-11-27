8eb370cc559d96abcd71b375e68f55ea6db488c2
Mluleki <melokuhlemluleki02@gmail.com>
2025-11-27 09:49:43 +0200

    feat:Created Error404.tsx and Error404.module.css for 404 error handling

---
+++ front-end/src/pages/user/Error404.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import styles from "./Error404.module.css";
+import Navbar from "../../components/NavBar/Navbar";
+import Footer from "../../components/footer/Footer";
+
+const Error404 = () => {
+  return (
+    <div>
+      {" "}
+      <Navbar />
+      <div className={styles.container}>
+        <h1 className={styles.code}>404</h1>
+
+        <h2 className={styles.title}>Page Not Found</h2>
+
+        <p className={styles.text}>
+          It looks like nothing was found at this location.
+        </p>
+
+        <Link to="/" style={{ marginTop: "30px" }}>
+          <Button variant="primary">BACK TO HOME</Button>
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
+};
