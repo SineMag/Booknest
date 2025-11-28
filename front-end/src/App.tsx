b245107e36eb5565c8753400dea056fdc6b07cb4
Shantela21 <shantelaslie@gmail.com>
2025-11-28 09:59:59 +0200

    fixed changes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,7 +7,7 @@ import UserLogin from "./pages/user/UserLogin";
 import Error404 from "../src/pages/user/Error404";
 import UserRegister from "./pages/user/UserRegister";
 import Booking from "./pages/user/Booking";
-import UserDashboard from "./pages/user/UserDashboard";
+import { UserDashboard } from "./pages/user/UserDashboard";
 import Confirmation from "./pages/user/Confirmation";
 import MyBookings from "./pages/user/MyBookings";
 import UserProfile from "./pages/user/UserProfile";
@@ -17,20 +17,12 @@ import AdminLogin from "./pages/admin/AdminLogin";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminRegister from "./pages/admin/AdminRegister";
 import InventoryManagement from "./pages/admin/InventoryManagement";
-<<<<<<< HEAD
 import AccommodationDetails from "./pages/user/AccomodationDetails";
-=======
-import ReservationManagement from "./pages/admin/ReservationManagement";
->>>>>>> 07f0c7ca8637b622a65b6d3826d064755cbc3d43
 // import AdminLogin from "./pages/admin/AdminLogin";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-=======
-      <Navbar />
->>>>>>> 07f0c7ca8637b622a65b6d3826d064755cbc3d43
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<UserLogin />} />
@@ -53,7 +45,6 @@ function App() {
 
         {/* Admin */}
 
-<<<<<<< HEAD
         <Route path="/adminRegister" element={<AdminRegister />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/adminDashboard" element={<UserDashboard />} />
@@ -65,27 +56,10 @@ function App() {
         {/* <Route path="/adminDashboard" element={<AdminDashboard />} /> */}
         <Route path="/inventoryManagement" element={<InventoryManagement />} />
         {/* <Route path="/reservationManagement" element={<ReservationManagement />} /> */}
-=======
-        <Route path="/admin-register" element={<AdminRegister />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route
-          path="/inventory-management"
-          element={<InventoryManagement />}
-        />
-        <Route
-          path="/reservation-management"
-          element={<ReservationManagement />}
-        />
->>>>>>> 07f0c7ca8637b622a65b6d3826d064755cbc3d43
 
         {/* <Route path="/adminLogin" element={<AdminLogin />} /> */}
         <Route path="*" element={<Error404 />} />
       </Routes>
-<<<<<<< HEAD
-=======
-      <Footer />
->>>>>>> 07f0c7ca8637b622a65b6d3826d064755cbc3d43
     </>
   );
 }
